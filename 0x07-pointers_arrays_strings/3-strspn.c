66c8da92ecbc3502a11a935b2bbf2a2e76e5e9fa
Dalbee <dalbertonyebuchi@gmail.com>
2023-04-13 04:11:33 -0700

    Write a function that gets the length of a prefix substring.
    Prototype: unsigned int _strspn(char *s, char *accept);
    Returns the number of bytes in the initial segment of s which consist only of bytes from accept
    FYI: The standard library provides a similar function: strspn. Run man strspn to learn more.

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - This fxn returns the no of bytes in s that only has bytes of accept
+ * @s: pointer input variable
+ * @accept: pointer input variable
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+			if (*s == accept[a])
+			{
+				n++;
+				break;
+			}
+			else if (accept[a + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
