09eaf4a08da3789ed9f65df3d7b41313eab28f59
Dalbee <dalbertonyebuchi@gmail.com>
2023-05-02 16:39:00 -0700

    This is a function that prints a listint_t linked list.
    Prototype: size_t print_listint_safe(const listint_t *head);
    Returns: the number of nodes in the list
    This function can print lists with a loop
    You should go through the list only once
    If the function fails, exit the program with status 98
    Details:
    This is a function from the lists.h header file in C that prints a linked list of integers safely, meaning that if there is a loop in the list, the function will stop and print a message indicating that there is a loop.
    
    It starts by declaring a variable called num_of_nodes to keep track of the number of nodes in the list. It also declares three pointers: hptr, new_node, and add. hptr is used to keep track of the nodes that have already been traversed, new_node is used to create new nodes for hptr, and add is used to traverse hptr.
    
    It then enters a loop that runs as long as head is not NULL, which means that there are still nodes to print.
    
    Inside the loop, it creates a new node for hptr that stores the address of the current node in the list (head) and adds it to the beginning of hptr.
    
    It then traverses hptr using the add pointer, checking if any of the nodes in hptr have the same address as the current node (head). If there is a match, it means that there is a loop in the list, so the function prints a message indicating this and frees the memory allocated for hptr. It then returns the number of nodes in the list that were printed before the loop was detected.
    
    If there is no loop, the function prints the address and value of the current node, increments num_of_nodes, and moves on to the next node.
    
    Once the loop is finished, the function frees the memory allocated for hptr and returns the total number of nodes printed.
    
    Overall, this function is a useful way to print a linked list safely, ensuring that the program doesn't get stuck in an infinite loop if the list has a loop.

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,74 @@
+#include "lists.h"
+
+/**
+ * free_listp - a fx. that frees a linked list
+ * @head: the head of a list.
+ *
+ * Return: no return because it is a void function.
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp_value;/**pointer temp*/
+	/**variable stores current node to be freed*/
+	listp_t *current;/**pointer to iterate through the list*/ 
+
+	if (head != NULL)
+	{
+		current = *head;
+		while ((temp_value = current) != NULL)
+		{
+			current = current->next;
+			free(temp_value);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * print_listint_safe - a fx. to safely print a linked list.
+ * @head: the head of a list.
+ *
+ * Return: the number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t num_of_nodes = 0;
+	listp_t *hptr, *new_node, *add; /**declares a variable called*/
+	/**num_of_nodes to keep track of the number of nodes in the list. Also, declare 
+	 * three pointers: hptr, new_node, and add. hptr is used to keep
+	 * track of the nodes that have already been traversed, new_node
+	 * is used to create new nodes for hptr, and add is used to traverse hptr.*/
+
+	hptr = NULL;
+	while (head != NULL)
+	{
+		new_node = malloc(sizeof(listp_t));
+
+		if (new_node == NULL)
+			exit(98);
+
+		new_node->p = (void *)head;
+		new_node->next = hptr;
+		hptr = new_node;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (num_of_nodes);
+			}
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		num_of_nodes++;
+	}
+
+	free_listp(&hptr);
+	return (num_of_nodes);
+}
