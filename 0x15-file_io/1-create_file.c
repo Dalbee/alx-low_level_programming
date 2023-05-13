f04d3eaaed3586b0bd6f67caea1ac32bf2b9b3a7
Dalbee <dalbertonyebuchi@gmail.com>
2023-05-12 17:32:23 -0700

    a function that creates a file.
    Prototype: int create_file(const char *filename, char *text_content);
    where filename is the name of the file to create and text_content is a NULL terminated string to write to the file
    Returns: 1 on success, -1 on failure (file can not be created, file can not be written, write “fails”, etc…)
    The created file must have those permissions: rw-------. If the file already exists, do not change the permissions.
    if the file already exists, truncate it
    if filename is NULL return -1
    if text_content is NULL create an empty file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,25 +10,29 @@
 int create_file(const char *filename, char *text_content)
 {
 	int my_file;
-	ssize_t num_bytes_written = 0;
+	int len = 0;
+	int num_bytes_written;
 
 	if (!filename)
-	/**filename==NULL*/
 		return (-1);
-	my_file = open(filename, O_WRONLY);
-	if (!my_file)
+
+	my_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if(!my_file)
 		return (-1);
-	if (text_content != NULL)
-	{
-		while (text_content[num_bytes_written] != '\0')
-			num_bytes_written++;
-		if (write(my_file, text_content, num_bytes_written)
-				!= num_bytes_written)
-		{
-			close(my_file);
-			return (-1);
-		}
-	}
+
+	if (!text_content)
+		text_content = "";
+
+	for (len = 0; text_content[len]; len++)
+		;
+
+	num_bytes_written = write(my_file, text_content, len);
+
+	if (num_bytes_written == -1)
+		return (-1);
+
 	close(my_file);
+
 	return (1);
 }
